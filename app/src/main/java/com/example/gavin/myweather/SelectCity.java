1e1e1b285e9d2c525219a82533cd2dc232e3c54f
PKUYUEWANG <www8478652110@163.com>
2017-11-15 11:09:41 +0800

    10th

--- app/src/main/java/com/example/gavin/myweather/SelectCity.java
+++ app/src/main/java/com/example/gavin/myweather/SelectCity.java
@@ -4,7 +4,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.edu.pku.gavin.app.MyApplication;
+import cn.edu.pku.gavin.bean.City;
 
 /**
  * Created by GAVIN on 2017/10/18.
@@ -12,11 +20,29 @@ import android.widget.ImageView;
 
 public class SelectCity  extends Activity implements View.OnClickListener{
     private ImageView mBackBtn;
+    private ListView cityListList;
+    private List<City> mCityList;
+    private MyApplication myApplication;
+    private ArrayList<String> mArrayList;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_city);
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+
+        myApplication=(MyApplication)getApplication();
+        mCityList=myApplication.getCityList();
+        mArrayList=new ArrayList<String>();
+        for(int i=0;i<mCityList.size();i++)
+        {
+            String cityName =mCityList.get(i).getCity();
+            mArrayList.add(cityName);
+        }
+        cityListList=(ListView)findViewById(R.id.selectcity_list);
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(SelectCity.this,android.R.layout.simple_list_item_1,mArrayList);
+
+        cityListList.setAdapter(adapter);
     }
     public void onClick(View v){
         switch (v.getId()){
